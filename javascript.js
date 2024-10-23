8146f35447ec444387894f47580cabe876916b8e
David Koy <dzilla398@gmail.com>
2024-10-23 12:00:44 +1030

    Added pseudo code for intital plan of devlopement of the project

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/*  // Initial Plan (ongoing changes)
+    // 1. Create a var that stores all the book objects (in array object)
+    
+    // 2. Book constructor that will have the following properties
+        * title of book
+        * author of the book
+        * number of pages
+        * status if user has read or not
+    
+    // 3. `addBook()` will display a form where user inputs this data
+        *  Use a `dialog` show a popbox that presents a form
+        *  Create a html form (in js or html?) used the properties from the Book constructor
+        *  Form will have a submit button and refresh button.
+            * if user selects submit button it will close the `dialog`
+            * if user selects refesh button it will remove all existing text in the form    
+
+    // 4. `displayBook()` will display the inputed form into one of the cards
+        * use if else statement to check if a user has read or not
+        * if user has read it will move into the completed card else move to the ongoing card section
+*/

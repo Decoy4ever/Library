02bb8e5e011630f06271cb4ccadb06e966591157
David Koy <dzilla398@gmail.com>
2024-10-28 15:55:51 +1030

    Modified the structure of the form in the dialog. Added a example entered Book form in the Book Collection located in the `grid-area: main`.
    Added styling to the input, buttons and resized the dimensions of these elements and removed previous classes for creating the cards.
    
    Added a a dialog event when user clicks on the add button. Prints a form and if the user presses "add" it create a new para.

--- javascript.js
+++ javascript.js
@@ -18,3 +18,102 @@
         * use if else statement to check if a user has read or not
         * if user has read it will move into the completed card else move to the ongoing card section
 */
+
+const modal = document.querySelector('.button-modal');
+const dialog = document.querySelector('.dialog');
+const closeBtn = document.querySelector('.exit-btn');
+const submitBtn = document.querySelector(".submit-btn");
+const ongoingCard= document.querySelector(".ongoing-card");
+const author = document.querySelector(".author")
+const title = document.querySelector(".title")
+const numberOfpgs = document.querySelector(".num_pgs")
+ 
+function Library()
+{
+    this.myLibraryOfBooks = []; // store the books as object inside here
+}
+
+// function constructor Book
+function Book(author, title, number_pg)
+{
+    this.title = title;
+    this.author = author;
+    this.number_pg = number_pg;
+}
+
+// Library to inherit the objects of Book
+Object.setPrototypeOf(Library.prototype, Book.prototype);
+
+// create a function called addBookToLibrary which is an object for Library().
+Library.prototype.addBookToLibrary = function(book)
+{
+    // the only property of addBookToLibray is adding books into the library
+    return this.myLibraryOfBooks.push(book);
+}
+
+// create a library constructor
+const lib = new Library();
+
+// function displayInfo(bk)
+Library.prototype.displayInfoBook = function(bk)
+{
+    // create a book constructor
+    bk = new Book(`${author.value}`,`${title.value}`,`${numberOfpgs.value}`)
+    
+    // add book to library
+    lib.addBookToLibrary(bk);
+
+    // display array of books
+    console.log(lib.myLibraryOfBooks)
+}
+
+
+function openCloseDialog()
+{
+    // open the dialog
+    modal.addEventListener('click',() => 
+    {
+        // open dialog
+        dialog.showModal();
+
+        // if usr presses submit btn event
+        submitBtn.addEventListener('click',(e) => 
+        {
+            lib.displayInfoBook();
+
+            // create a HTML
+            const form = document.createElement('form');
+            // const labelForm = document.createElement('label');
+            // const inputForm = document.createElement('input');
+
+            const div = document.createElement('div');
+            const classAttName = document.createAttribute('class');
+            classAttName.value = "card";
+            div.setAttributeNode(classAttName);
+            const textContent = document.createTextNode(`${author.value}, ${title.value}, ${numberOfpgs.value}`);
+            div.appendChild(textContent);
+
+
+            document.querySelector('.form-container-external').insertAdjacentElement("afterend", div);
+        })
+        
+    })
+
+    // close the form
+    closeBtn.addEventListener('click',(e) => 
+    {
+        e.preventDefault();
+        dialog.close();
+    })
+}
+
+// function constructor Library
+openCloseDialog()
+
+
+
+
+
+
+
+

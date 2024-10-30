176ede3ab844bb2db569fe67cfd7dfea7024b222
David Koy <dzilla398@gmail.com>
2024-10-30 15:25:57 +1030

    Removed the card class for the div css file. Modified the js file to dynamically create the card with user inputs and display the card. Added flex on the cards for formatiing in css file.

--- javascript.js
+++ javascript.js
@@ -24,51 +24,113 @@ const modal = document.querySelector('.button-modal');
 const closeBtn = document.querySelector('.exit-btn');
 const submitBtn = document.querySelector(".submit-btn");
 const mainCard = document.querySelector(".main-card");
-// const card = document.querySelector(".card")
 
-const author = document.querySelector(".author")
-const title = document.querySelector(".title")
-const numberOfpgs = document.querySelector(".pages")
+const author_input = document.querySelector(".author");
+const title_input = document.querySelector(".title");
+const pages_input = document.querySelector(".pages");
+const status_input = document.querySelector(".status")
+
  
+// function constructor library
 function Library()
 {
-    this.myLibraryOfBooks = []; // store the books as object inside here
+     // store the books as object inside here
+    this.myLibraryOfBooks = [];
 }
 
 // function constructor Book
-function Book(author, title, number_pg)
+function Book(author, title, pages, status)
 {
     this.title = title;
     this.author = author;
-    this.number_pg = number_pg;
+    this.pages = pages;
+    this.status = status
 }
 
 // Library to inherit the objects of Book
 Object.setPrototypeOf(Library.prototype, Book.prototype);
 
-// create a function called addBookToLibrary which is an object for Library().
-Library.prototype.addBookToLibrary = function(book)
-{
-    // the only property of addBookToLibray is adding books into the library
-    return this.myLibraryOfBooks.push(book);
-}
-
-// create a library constructor
+// create a library instance
 const lib = new Library();
 
-// function displayInfo(bk)
-Library.prototype.displayInfoBook = function(bk)
+// create a function called addBookToLibrary() which is a property for Library(). Check Object.getPrototypeOf(Library.prototype)
+Library.prototype.addBookToLibrary = function(book)
 {
-    // create a book constructor
-    bk = new Book(`${author.value}`,`${title.value}`,`${numberOfpgs.value}`)
+    // retreive data from forms
+    this.title = `${title_input.value}`;
+    this.author = `${author_input.value}`;
+    this.pages = `${pages_input.value}`;
+    this.status = `${status_input.value}`;
     
-    // add book to library
-    lib.addBookToLibrary(bk);
+    // create a instance of the book object using this data
+    book = new Book(this.title, this.author, this.pages, this.status);
 
-    // display array of books
-    console.log(lib.myLibraryOfBooks)
+    // the only property of addBookToLibray is adding books into the library array
+    this.myLibraryOfBooks.push(book);
 }
 
+// function displayInfo(bk)
+Library.prototype.displayInfoBook = function(book)
+{
+    // add book to library instance and call the method addBookToLibrary
+    lib.addBookToLibrary(book);
+
+    // loop throught the books and print one each
+    lib.myLibraryOfBooks.forEach((indexBk) => 
+    {    
+        const div = document.createElement('div');
+        const classAttName = document.createAttribute('class');
+        classAttName.value = "card";
+        div.setAttributeNode(classAttName); 
+
+        // create and set the Header
+        const headerBk = document.createElement('h3');
+        
+        headerBk.textContent = indexBk.title;
+        div.appendChild(headerBk);
+
+        // create a two more divs 
+        const paraAuthor = document.createElement('p');
+        paraAuthor.textContent = `Author: ${indexBk.author}`
+        div.appendChild(paraAuthor);
+
+        const paraNumOfpgs = document.createElement('p');
+        paraNumOfpgs.textContent = `Pages: ${indexBk.pages}`
+        div.appendChild(paraNumOfpgs);
+
+        // create button
+        const statusBtn = document.createElement('button');
+        statusBtn.textContent = `${indexBk.status}`;
+        console.log(statusBtn.textContent)
+        statusBtn.addEventListener('click',() => 
+        {
+            if(statusBtn.textContent === "Completed")
+            {
+                statusBtn.textContent = "Ongoing";
+                console.log(statusBtn.textContent);
+            }
+            else if(statusBtn.textContent = "Ongoing")
+            {
+                statusBtn.textContent = "Completed"
+                console.log(statusBtn.textContent)
+            }
+        })
+
+        div.appendChild(statusBtn);
+        mainCard.appendChild(div);
+        
+        // check the current library
+        lib.myLibraryOfBooks[indexBk];
+    })
+    console.log(lib.myLibraryOfBooks);
+
+}
+
+// Probelm user enter a new book it displays all of the elements in book which duplicate more objects
+
+
+// openCloseDialog() inherit the properties of Library
+Object.setPrototypeOf(openCloseDialog.prototype, Library.prototype)
 
 function openCloseDialog()
 {
@@ -77,47 +139,30 @@ function openCloseDialog()
     {
         // open dialog
         dialog.showModal();
-
+        
         // if usr presses submit btn event
         submitBtn.addEventListener('click',(e) => 
         {
-            lib.displayInfoBook();
-            const div = document.createElement('div');
-            const classAttName = document.createAttribute('class');
-            // retrieve the card class and set it to the div
-            classAttName.value = "card";
-            div.setAttributeNode(classAttName);
-
-            // create and set the Header
-            const headerBk = document.createElement('h3');
-            headerBk.textContent = `${title.value}`;
-            div.appendChild(headerBk);
-
-            // create a two more divs 
-            const paraAuthor = document.createElement('p');
-            paraAuthor.textContent = `Author: ${author.value}`
-            div.appendChild(paraAuthor);
-
-            const paraNumOfpgs = document.createElement('p');
-            paraNumOfpgs.textContent = `Pages: ${numberOfpgs.value}`
-            div.appendChild(paraNumOfpgs);
-
-            mainCard.appendChild(div);
+            lib.displayInfoBook();        
         })
         
     })
-
+    
     // close the form
     closeBtn.addEventListener('click',(e) => 
     {
         e.preventDefault();
         dialog.close();
     })
+
+    title_input.value = "";
+    author_input.value = "";
+    pages_input.value = "";
+
+        
 }
 
-// function constructor Library
 openCloseDialog()
-
 
 
 
